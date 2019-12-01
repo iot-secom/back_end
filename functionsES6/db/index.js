f13fcd979cf683246ba3bd6780f7fccdf35c3fd3
Q00 <jqyu.lee@gmail.com>
2019-12-02 01:41:49 +0900

    feat: ⚡️ db connect

--- functionsES6/db/index.js
+++ functionsES6/db/index.js
@@ -2,21 +2,16 @@ import * as functions from 'firebase-functions';
 import fs from 'fs';
 import mongoose from 'mongoose';
 
-const functionConfig = () => {
-	if (process.env.RUN_LOCALLY) {
-		return JSON.parse(fs.readFileSync('.env.json'));
-	} else {
-		return functions.config();
-	}
-};
 const connectDB = () => {
-	const { MONGO_URI } = functionConfig().env;
-	mongoose.Promise = global.Promise;
+  const MONGO_URI = `mongodb+srv://admin:${
+    functions.config().env.pwd
+  }@cluster0-kxzuv.mongodb.net/test?retryWrites=true&w=majority`;
+  mongoose.Promise = global.Promise;
 
-	return mongoose
-		.connect(MONGO_URI, { useNewUrlParser: true })
-		.then(() => console.log('connected to db!'))
-		.catch((e) => console.log(e));
+  return mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log('connected to db!'))
+    .catch(e => console.log(e));
 };
 
 export default connectDB;
