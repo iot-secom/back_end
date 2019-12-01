1eb00ee143737dcca80c5d02e971b7b0304d8a5c
Q00 <jqyu.lee@gmail.com>
2019-12-02 01:43:00 +0900

    feat: ⚡️make register ,pwd, who_on, exit module

--- functionsES6/controller/indexCtrl.js
+++ functionsES6/controller/indexCtrl.js
@@ -1,5 +1,6 @@
-import * as functions from "firebase-functions";
-import { _bucketWithOptions } from "firebase-functions/lib/providers/storage";
+import * as functions from 'firebase-functions';
+import { _bucketWithOptions } from 'firebase-functions/lib/providers/storage';
+import { User, Log } from '../db/models';
 
 class Controller {
   async register(req, res) {
@@ -7,18 +8,49 @@ class Controller {
       const nickname = req.body.name;
       const mac = req.body.mac;
       const pwd = req.body.pwd;
-      res.json({ res: `nickname: ${nickname}, mac: ${mac}, pwd: ${pwd}` });
-    } catch (err) {}
+      const user = await User.findOne({ device_id: mac });
+      if (!user) {
+        const newUser = new User();
+        newUser.name = nickname;
+        newUser.pwd = pwd;
+        newUser.device_id = mac;
+        newUser.exist_flag = false;
+        await newUser.save();
+      }
+      res.json({ status: 'ok' });
+    } catch (err) {
+      console.log(err);
+    }
   }
   async pwd_match(req, res) {
     try {
       //db에서 mac address 와 pwd를 서로 매치
       //매치가 되면 디비에서 들어왔는지 플래그 수정
       //나갈때는 안드로이드에서 버튼
-      res.json({ res: `your pwd is ${req.body.pwd}` });
+      const mac = req.body.mac;
+      const user = await User.findOne({ device_id: mac });
+      if (user) {
+        if (user.pwd == req.body.pwd) {
+          const update_user = await User.updateOne(
+            { device_id: mac },
+            { $set: { exist_flag: 'true' } }
+          );
+          const log = new Log();
+          log.device_id = mac;
+          log.exist_flag = true;
+          log.time = new Date().toString();
+          await log.save();
+          console.log(log);
+          res.json({ user: user.nickname, exist_flag: true });
+        } else {
+          res.json({ user: user.nickname, status: 'pwd is not match' });
+        }
+      } else {
+        res.json({ user: user.nickname, status: 'no user' });
+      }
     } catch (err) {
       console.log(err);
-      res.json({ status: "error", err: err });
+      res.json({ status: 'error', err: err });
     }
   }
 
@@ -28,14 +60,18 @@ class Controller {
       res.json({ res: `your mac address is ${req.body.mac}` });
     } catch (err) {
       console.log(err);
-      res.json({ status: "error", err: err });
+      res.json({ status: 'error', err: err });
     }
   }
 
   async who_on(req, res) {
     try {
-      res.json({ name: ["김주영", "이재규"] });
-    } catch (err) {}
+      const users = await User.find({ exist_flag: true }, { name: 1 });
+
+      res.json({ users: users });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async log(req, res) {
@@ -47,7 +83,16 @@ class Controller {
 
   async exit(req, res) {
     try {
-      res.json({ exit: "ok" });
+      const mac = req.body.mac;
+      const user = await User.findOne({ device_id: mac });
+      if (user) {
+        User.updateOne({ device_id: mac }, { $set: { exist_flag: 'false' } });
+        const log = new Log();
+        log.device_id = mac;
+        log.exist_flag = false;
+        log.time = new Date().toString();
+      }
+      res.json({ exit: 'ok' });
     } catch (err) {}
   }
 }
